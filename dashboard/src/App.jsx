0c835faba6f1bd6d66864f9d97140b2b0b579d55
SubhradeepDE <work.subhradeep030802@gmail.com>
2026-09-18 23:26:58 +0530

    Base URL connected to production API

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -9,7 +9,7 @@ import {
 
 import "leaflet/dist/leaflet.css";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://open-city-mobility-api.onrender.com";
 
 function App() {
   const [metrics, setMetrics] = useState(null);
